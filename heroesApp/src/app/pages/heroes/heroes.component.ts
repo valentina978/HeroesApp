dd596227b329b2095be8dcc3e62cbfc0fdd19f43
valentina978 <114037877+valentina978@users.noreply.github.com>
2023-11-09 07:05:16 -0500

    commit 10,viddeo14

--- heroesApp/src/app/pages/heroes/heroes.component.ts
+++ heroesApp/src/app/pages/heroes/heroes.component.ts
@@ -2,6 +2,7 @@
 import { Component,OnInit } from '@angular/core';
 import { HeroesService } from 'src/app/services/heroes.service';
 import { HeroeModel } from 'src/app/models/heroe.model';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-heroes',
@@ -11,14 +12,27 @@ import { HeroeModel } from 'src/app/models/heroe.model';
 export class HeroesComponent implements OnInit {
 
   heroes:HeroeModel[]=[]; 
-constructor(private heroesService:HeroesService){
-}
+constructor(private heroesService:HeroesService){}
+
 
   ngOnInit(){
     this.heroesService.getHeroes()
-    .subscribe(resp=>{
-      console.log(resp);
-      this.heroes=resp;
-    });
+    .subscribe(resp=>this.heroes=resp);
   }
+
+    borrarHeroe(heroe:HeroeModel,i:number){
+      Swal.fire({
+        title: 'Esta seguro?',
+        text:  `Esta seguro de que desea borrar a ${heroe.nombre}`,
+        icon: 'question',
+        showCancelButton: true,
+        showConfirmButton: true
+      });
+
+      this.heroes.splice(i,1);
+      /* this.heroesService.borrarHeroe(heroe.id).subscribe(); */
+
+    }
+
+  
 }
